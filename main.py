f1c9af577717c9071b6009fd1e1f0eaa0f8c82ab
Danijel Stanić <danijel.stanich@gmail.com>
2024-06-12 13:12:34 +0200

    add count_characters function

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    char_count = count_characters(text)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_characters(text):
+    char_count = {}
+
+    for char in text:
+        lowercase_char = char.lower()
+        if lowercase_char.isalpha():
+            if lowercase_char not in char_count:
+                char_count[lowercase_char] = 1
+            else:
+                char_count[lowercase_char] += 1
+
+    return char_count
+
+main()
